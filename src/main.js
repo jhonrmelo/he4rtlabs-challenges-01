ddf94c84c428689a61501a4434d6a7460fcd04fa
jhonrmelo <45439356+jhonrmelo@users.noreply.github.com>
2020-02-22 20:26:36 -0300

    Fim projeto

---
+++ src/main.js
@@ -0,0 +1,58 @@
+
+let viewRegras = document.querySelector('.infoContent');
+let viewCalc = document.querySelector('.valueContent');
+let headerTitle = document.getElementById('headerTitle');
+let inputTempo = document.getElementById('inputTempo');
+let inputDiasEfetivos = document.getElementById('inputDiasEfetivos');
+let inputFerias = document.getElementById('inputFerias');
+let inputValor = document.getElementById('inputValor');
+let spanValue = document.getElementById('spanValue');
+
+document.getElementById('btnViewCalc').addEventListener('click', () => {
+    headerTitle.innerHTML = 'Calculadora';
+    viewCalc.classList.remove('display-none');
+    viewRegras.classList.add('display-none');
+});
+
+document.getElementById('btnViewRegras').addEventListener('click', () => {
+    headerTitle.innerHTML = 'Regras';
+    viewCalc.classList.add('display-none');
+    viewRegras.classList.remove('display-none');
+});
+
+inputTempo.addEventListener('input', ExecutaCalculo);
+inputDiasEfetivos.addEventListener('input', ExecutaCalculo);
+inputFerias.addEventListener('input', ExecutaCalculo);
+inputValor.addEventListener('input', ExecutaCalculo);
+
+function ValidaInputs() {
+    if (inputTempo.value === '' || inputFerias.value === '' || inputValor.value === '' || inputDiasEfetivos.value === '')
+        return false;
+    return true;
+}
+
+function ExecutaCalculo() {
+
+    if (ValidaInputs()) {
+        let Dias  = inputDiasEfetivos.value;
+        let Tempo = inputTempo.value;
+        let Valor = inputValor.value;
+        let Ferias  = inputFerias.value;
+        var ValorFinal = (Valor / (Dias * 4 * Tempo)) + ((Ferias * Dias * Tempo));
+        console.log(ValorFinal);
+        spanValue.innerHTML = `R$ ${ValorFinal.toFixed(2)}`
+    }
+    else {
+        spanValue.innerHTML = `R$ 0`
+    }
+}
+
+function isNumberKey(evt)
+{
+   var charCode = (evt.which) ? evt.which : event.keyCode
+   if (charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+
+   return true;
+}
+
